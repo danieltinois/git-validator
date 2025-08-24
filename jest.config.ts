a8b8bfa55ba7d09c5097dbb3fd5b63468afef2b8
danieltinois <danieltinois@gmail.com>
2025-08-24 16:43:53 -0300

    docs: improve README with updated configuration, examples, and test instructions
    - Added details on `.gitvalidatorrc` configuration.
    - Included examples for commit and branch auto-suggestions.
    - Documented test instructions and Jest usage.
    - Updated roadmap with new features and progress.

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,14 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
